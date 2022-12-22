c81fbb4a08c8c245366a783c302a4db70baf085a
FERNANDO LUIS DE OLIVEIRA SALDANHA <75080506+NandoDev-lab@users.noreply.github.com>
2022-12-22 17:08:01 -0300

    movimentação implementada
    foi colocado movimentação no inimigo helicoptero

--- js/engine.js
+++ js/engine.js
@@ -1,46 +1,34 @@
+
 function start() { // Inicio da função start()
 
 	$("#inicio").hide();
 	
 	$("#fundoGame").append("<div id='jogador' class='anima1'></div>");
 	$("#fundoGame").append("<div id='helicoptero' class='anima2'></div>");
-	$("#fundoGame").append("<div id='helicoptero' class='anima4'></div>");
 	$("#fundoGame").append("<div id='inimigo2'></div>");
 	$("#fundoGame").append("<div id='amigo' class='anima3'></div>");
-
-
-
-//Principais variáveis do jogo
-	
-
-
-
-
 
 
 //Função que movimenta o fundo do jogo
 
 function movefundo() {
+    esquerda = parseInt($("#fundoGame").css("background-position"));
+    $("#fundoGame").css("background-position",esquerda-1);
 
-esquerda = parseInt($("#fundoGame").css("background-position"));
-$("#fundoGame").css("background-position",esquerda-1);
+} 
 
-} // fim da função movefundo()
-function moveInimigos(){
-    frente = parseInt($("#helicoptero").css("background-position"));
-    $("#helicoptero").css("background-position",frente);
-}
-    //Verifica se o usuário pressionou alguma tecla	
+ //Verifica se o usuário pressionou alguma tecla	
+    
 var jogo = {}
 	
     //Game Loop
-    var TECLA = {
-        W: 87,
-        S: 83,
-        D: 68,
-        A: 65,
-        end: 35
-        }
+var TECLA = {
+    W: 87,
+    S: 83,
+    D: 68,
+    A: 65,
+    end: 35
+}
     
         jogo.pressionou = [];
         
@@ -54,35 +42,76 @@ $(document).keyup(function(e){
 });
 
 // função de controle do Jogador
-function movejogador() {	
+function movejogador() {
+    var topo;
+    var volta;
+    var avanca;	
+    //movimentação para cima
     if (jogo.pressionou[TECLA.W]) {
     var topo = parseInt($("#jogador").css("top"));
-    $("#jogador").css("top",topo-10);            
+    $("#jogador").css("top",topo-10);  
+    if(topo<=1){
+        $("#jogador").css("top",topo+10); 
+    }          
 }
-     // movimentação para cima ou para baixo       
+     // movimentação para baixo       
     if(jogo.pressionou[TECLA.S]){                
     var topo = parseInt($("#jogador").css("top"));
-    $("#jogador").css("top",topo+10);	
+    $("#jogador").css("top",topo+10);
+    if(topo>=434){
+        $("#jogador").css("top",topo-10);
+    }	
+    
+}   
+    // função voltar na tela
+    if(jogo.pressionou[TECLA.A]){                
+    var volta = parseInt($("#jogador").css("left"));
+    $("#jogador").css("left",volta-10);	
+    if (volta < 0){
+        $("#jogador").css("left",volta+10);
+    }
 }
-if(jogo.pressionou[TECLA.A]){                
-    var voltar = parseInt($("#jogador").css("left"));
-    $("#jogador").css("left",voltar-10);	
-}
-    //função de disparo
+    //função de avançar na tela
     if (jogo.pressionou[TECLA.D]){
-        var avancar = parseInt($("#jogador").css("left"));
-        $("#jogador").css("left", avancar+10);                
-              
-}
+        var avanca = parseInt($("#jogador").css("left"));
+        $("#jogador").css("left", avanca+10);                
+        if(avanca>690){
+            $("#jogador").css("left", avanca-10);
+        }      
+    }
         
 } // fim da função movejogador()
+
+// função movimentação inimigo
+
+function moveinimigo1() {
+
+    var velocidade=5;   
+    var posicaoY = parseInt(Math.random() * 334);
+	posicaoinicial = parseInt($("#helicoptero").css("left"));
+    var posicaoatual = posicaoinicial-velocidade;   
+	$("#helicoptero").css("left", posicaoatual );		
+		if (posicaoatual <= 0) {  
+            var proximo = posicaoY;                 
+		    $("#helicoptero").css("left", 680 );
+            $("#helicoptero").css("top",proximo); 
+               
+		   
+	} 
+         
+} //Fim da função moveinimigo1()
+
+
 jogo.timer = setInterval(loop,30);
 
 function loop() {
 movejogador();
 movefundo();
-moveInimigos();
+moveinimigo1();
 
 } // Fim da função loop()
+
+
+
 
 } // Fim da função start
